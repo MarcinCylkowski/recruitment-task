ce4603f26eaa0930ec6a6af84d547a1ca08f32bf
Marcin <marcin.cylkowski3@gmail.com>
2020-08-25 08:54:53 +0200

    added jar file

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,13 +1,13 @@
 package src;
 
 import java.io.IOException;
-import static src.LocalWeather.connectWithApi;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        connectWithApi();
+        LocalWeather.connectWithApi();
 
     }
 
